5609b69680128cb5b491fdcb2dce0208b1bcccbf
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-14 15:37:44 +0600

    mongodb server connect

--- index.js
+++ index.js
@@ -3,15 +3,34 @@ const app = express()
 const cors = require('cors');
 const port =process.env.PORT || 5000
 require('dotenv').config() 
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
 
+const uri =`mongodb+srv://doctor-portal:ISsdPmGjJWdAUwd1@cluster0.8uxou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function  run (){
+  try{
+    const doctorCollection = client.db("doctor-portal").collection("service");
+    await client.connect();
+    app.get('/service', async (req, res) =>{
+      const query ={}
+      const cursor = doctorCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+  }finally{
+
+  }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('I am doctor portal')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Doctor portal service center ${port}`)
 })
