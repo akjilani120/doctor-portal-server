ea4d177191995bbef702d7c7d8003871098cd89c
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-16 18:39:04 +0600

    data exits added

--- index.js
+++ index.js
@@ -9,18 +9,33 @@ app.use(cors())
 app.use(express.json())
 
 const uri =`mongodb+srv://doctor-portal:ISsdPmGjJWdAUwd1@cluster0.8uxou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri)
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ const doctorCollection = client.db("doctor-portal").collection("service");
+ const bookingCollection = client.db("doctor-portal").collection("bookings");
 async function  run (){
   try{
-    const doctorCollection = client.db("doctor-portal").collection("service");
+   
     await client.connect();
     app.get('/service', async (req, res) =>{
       const query ={}
       const cursor = doctorCollection.find(query)
       const result = await cursor.toArray()
       res.send(result)
+    })
+    app.post('/booking', async(req, res) =>{
+      const booking = req.body;
+      console.log(booking)
+
+      const query = {treatment:booking.treatment, date:booking.date, patient:booking.patient}
+      console.log(query)
+       const exits = await bookingCollection.findOne(query)
+       
+       if(exits){
+         return res.send({sucess:false, booking:exits})
+       }
+      const result = await bookingCollection.insertOne(booking)
+       return res.send({sucess:true , result})
     })
   }finally{
 
