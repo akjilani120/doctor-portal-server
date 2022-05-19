d59b3d444f4029e0527cbad8a63ce634ae27f0e7
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-19 10:36:42 +0600

    added admin page

--- index.js
+++ index.js
@@ -124,6 +124,12 @@ async function  run (){
       }
      
     })
+    app.get('/admin/:email', async(req , res) =>{
+      const email = req.params.email;
+      const user  = await userCollection.findOne({email: email})
+      const isAdmin = user.role ==="admin";
+      res.send({admin : isAdmin})
+    })
   }finally{
 
   }
